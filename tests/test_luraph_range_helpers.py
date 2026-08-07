413469ee892928edfcb71b70f558df537accc97d
binxgtl <212375879+binxgtl@users.noreply.github.com>
2026-08-08 00:57:35 +0700

    test: cover real parenthesized range helper syntax

--- tests/test_luraph_range_helpers.py
+++ tests/test_luraph_range_helpers.py
@@ -4,6 +4,7 @@ from luauvmp import luraph_nested_helper_literals as helpers
 def test_infers_table_end_start_argument_order():
     source = '''
         d[27]=(function(M,p,A)
+            if d[26]~=d[16] then else d[3]=d[2] or d[14]; end;
             A=A or 1.0;
             p=p or #M;
             if (p-A+1)>7997 then
@@ -16,10 +17,10 @@ def test_infers_table_end_start_argument_order():
     assert helpers.infer_range_helpers(source) == {27: (0, 2, 1)}
 
 
-def test_infers_end_table_start_argument_order():
+def test_infers_end_table_start_argument_order_with_parenthesized_default():
     source = '''
         G[25]=function(y,p,j)
-            j=j or 1;
+            if G[1]~=G[24] then j=(j or 1); end;
             y=y or #p;
             if not((y-j+1)>7997) then
                 return G[11](p,j,y);
@@ -31,11 +32,11 @@ def test_infers_end_table_start_argument_order():
     assert helpers.infer_range_helpers(source) == {25: (1, 2, 0)}
 
 
-def test_infers_end_start_table_with_numeric_separators_and_identifier_underscores():
+def test_infers_parenthesized_table_end_start_table_with_numeric_separators():
     source = '''
-        U[0x1_a]=function(end_value,start_value,table_value)
+        (U)[0x1_a]=function(end_value,start_value,table_value)
             start_value=start_value or 0b0_1;
-            end_value=end_value or #table_value;
+            end_value=(end_value or #table_value);
             if not((end_value-start_value+0x0_1)>0x1_F3D) then
                 return U[21](table_value,start_value,end_value);
             else
@@ -55,6 +56,10 @@ def test_rewrites_range_calls_using_recovered_argument_roles():
         'return A[25](x+o[u]-2,R,x);',
         {25: (1, 2, 0)},
     ) == 'return table.unpack(R,x,x+o[u]-2);'
+    assert helpers._rewrite_range_calls(
+        '(A)[26](finish,start,R)',
+        {26: (2, 1, 0)},
+    ) == 'table.unpack(R,start,finish)'
 
 
 def test_balanced_arguments_are_not_split_inside_nested_calls():
